cd48f68844d73e2744bb84e83fc3c8d644a5bb36
srinivasGokarla <srinivasgokarla@gmail.com>
2024-01-06 18:44:05 +0530

    update

---
+++ backend/src/models/TEsting.js
@@ -0,0 +1,14 @@
+Array.prototype.groupBy = function(fn) {
+    console.log(this)
+   for(let i= 0; i < this.length; i++) {
+   Object.values(this[i] === 1)
+   }
+};
+
+const arr = new Array(5)
+arr.push([{"id" : 1},
+{"id" : 1},
+{"id" : 2},
+{"id" : 1}])
+
+arr.groupBy()
